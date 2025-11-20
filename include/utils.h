9aabb01e65d27f5a0b199a2c2c25b32af80dd62f
ducklin404 <duonght404@gmail.com>
2025-11-20 20:55:37 +0700

    refactor: remove unesecary

--- include/utils.h
+++ include/utils.h
@@ -6,6 +6,3 @@ String extractJsonStringValue(const String &json, const String &key);
 String extractJsonRawValue(const String &json, const String &key);
 bool parseBoolToken(const String &token, bool &ok);
 float parseFloatToken(const String &token, bool &ok);
-String extractObjectBody(const String &msg, const String &keyName);
-void applyAttributeKeyValue(const String &key, const String &value);
-void parseAndApplyObject(const String &obj);
